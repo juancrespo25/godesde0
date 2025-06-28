30502358f6e2a497114d79c709e74216b165d543
juancrespo25 <jcrespovirhuez@gmail.com>
2025-06-27 23:02:24 -0500

    trabajando con variables

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"curso/godesde0/variables"
+	"fmt"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	//variables.MuestroEnteros()
+	//variables.RestoVariables()
+
+	estado, texto := variables.ConviertoATexto(1573)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
